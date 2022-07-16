a565198098c4c83a7867ac17ed05953f023ae30a
jcarlosCalatayud <calatayud@fi.unju.edu.ar>
2022-07-15 22:13:25 -0300

    app completa

---
+++ app.py
@@ -0,0 +1,18 @@
+#from crypt import methods
+from flask import Flask, render_template, flash, request
+
+app = Flask(__name__)
+app.secret_key = "la_changa"
+
+@app.route('/hello')
+def index():
+    flash("what's your name?")
+    return render_template("index.html")
+
+@app.route('/greet', methods=["POST", "GET"])
+def greet():
+    flash("Hi " + str(request.form['name_input']) + ", great to see you!")
+    return render_template("index.html")
+
+if __name__ == '__main__':
+    app.run()
